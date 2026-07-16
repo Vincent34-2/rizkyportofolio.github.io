9062ec056a7a75a886365621b05cd1e754ee56d0
Rizky <isniyanto34@gmail.com>
2026-07-17 05:20:03 +0700

    Hapus path github pages yang tersisa

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  envPrefix: ['VITE_', 'REACT_APP_'],
-  build: {
-    outDir: 'build',
-    sourcemap: false,
-  },
-});
+  base: '/', 
+})
